59faf3891677c3bb0a436dd63b21adc1d89dc628
ThanhCuD <thanhduc552018@gmail.com>
2020-03-22 20:55:30 +0700

    new

--- CanvasFolder/Brick/main.js
+++ CanvasFolder/Brick/main.js
@@ -19,6 +19,9 @@ function direction(e){
     else if(code ==39){
         d = 'R'
     }
+    else if(code==32){
+        d='S'
+    }
 }
 
 // y = 480;
@@ -26,11 +29,19 @@ function distaincePointToLine(x,y,a,b,c){
     var result = Math.abs(a*x+b*y+c)/Math.sqrt(a*a+b*b);
     return result;
 }
+function checkCollision(rect1,rect2){
+    var result = (rect1.x < rect2.x + rect2.width &&
+    rect1.x + rect1.width > rect2.x &&
+    rect1.y <= rect2.y + rect2.height &&
+    rect1.y + rect1.height >= rect2.y);
+    return result;
+}
 function draw(){
     ctx.clearRect(0,0,480,480);
     ctx.strokeRect(0,0,480,480);
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "green";
     ctx.fillRect(shape.x,shape.y,box*2,box*2);
+    
     if(d=='L'){
         shape.x-=box;
         d = 'None';
@@ -39,10 +50,34 @@ function draw(){
         shape.x +=box;
         d = 'None';
     }
+    
     var dis = distaincePointToLine(shape.x,shape.y,0,1,-480);
     ctx.font = "30px Arial";
     ctx.fillText(dis,5*box,5*box);
-    
+
+    for (let i = 0; i < arr.length; i++) {
+        var rect1  = {
+            x: shape.x, y: shape.y, width: box*2, height: box*2
+        };
+        var rect2  = {
+            x: arr[i].x, y: arr[i].y, width: box*2, height: box*2
+        }
+        var result = checkCollision(rect1,rect2);
+        console.log(result);
+        if(result){
+            let nShape = {
+                x: shape.x,
+                y: shape.y
+            }
+            arr.push(nShape);
+            shape = {
+                x:7*box,
+                y:0*box
+            }        
+        }
+    }
+
+
    if(distaincePointToLine(shape.x,shape.y,0,1,-480)==box*2){
         let nShape = {
             x: shape.x,
@@ -57,7 +92,11 @@ function draw(){
     for (let i = 0; i < arr.length; i++) {
         ctx.fillRect(arr[i].x,arr[i].y,box*2,box*2);
     }
-    shape.y+=box;
+    
+    if(d!='S'){
+        shape.y+=box;
+    }
+    
 
 }
 
