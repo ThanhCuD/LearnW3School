8dc2e71ae14baefb428d162d7e3e92881e8b6a59
ThanhDuc <thanhduc552018@gmail.com>
2020-11-30 17:31:41 +0700

    new

---
+++ CaroGame/main.js
@@ -0,0 +1,16 @@
+// current == false => X : O;
+var isXCheck = false;
+var count = 0;
+var rowTotal = 10;
+var columnTotal = 10;
+var nWin = 5;
+
+window.onload = function(){
+    document.getElementById("main").innerHTML = render(rowTotal,columnTotal);
+};
+
+$( document ).ready(function() {
+    $('button').click(function(){
+        Check($(this));
+    })
+});
---
+++ CaroGame/method.js
@@ -0,0 +1,51 @@
+function render(row,column){
+    var resume = "<table cellpadding='0' cellspacing='0'";
+    for (let i = 0; i < row; i++) {
+        resume +="<tr>";
+        for (let j = 0; j < column; j++) {
+            resume += "<td>";
+            resume +="<button id='but"+i+"_"+j+"' row='"+i+"'col='"+j+"'></button>";
+            resume +="</td>"; 
+        }
+        resume +="</tr>"
+    }
+    resume+="</table>";
+    return resume;
+}
+function Check(button){
+    console.log(button);
+    if(isXCheck){
+        button.text("X");
+        CheckWin(button,"X");
+        isXCheck=!isXCheck;
+    }else{
+        button.text("O");
+        CheckWin(button,"O");
+        isXCheck=!isXCheck;
+    }
+    count++;
+}
+// col=row=diag=rdiag=0
+// winner=false
+// for i=1 to n
+//   if cell[x,i]=player then col++
+//   if cell[i,y]=player then row++
+//   if cell[i,i]=player then diag++
+//   if cell[i,n-i+1]=player then rdiag++
+// if row=n or col=n or diag=n or rdiag=n then winner=true
+function CheckWin(button,text){
+    var row=col=diag=rdiag=0;
+    let x = button.attr('row');
+    let y =  button.attr('col');
+    for (let i = 0; i < rowTotal; i++) {
+        if($("#but"+x+"_"+i).text()==text) {
+            col++;
+        }
+        if($("#but"+i+"_"+y).text()==text) {
+            row++;
+        }
+    }
+    if(row==nWin|| col==nWin || diag==nWin || rdiag==nWin) {
+        console.log(text + "Win");
+    }
+}
